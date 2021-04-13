b3a07a84839cf4e23d6b32abe07e39c90f3da904
Miles Maloney <49667452+milesmaloney@users.noreply.github.com>
2021-04-13 10:43:33 -0700

    Updated to integrate maps client
    updated to get full route instructions from google maps api through our maps client.

--- __init__.py
+++ __init__.py
@@ -1,13 +1,29 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+from mapsClient import mapsClient
 
 class TransitRouting(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
+    #gets the route to a destination from the current location or a given departure location [destination keyword REQUIRED]
     @intent_file_handler('routing.transit.intent')
     def handle_routing_transit(self, message):
-        self.speak_dialog('routing.transit')
+        #initializes/retrieves client (singleton)
+        gmaps = mapsClient.getClient()
+
+        #gets the keyword values from the utterance
+        departure = message.data.get('departure')
+        destination = message.data.get('destination')
+
+        #sets departure as here when there is no departure keyword as specified in mapsClient.py
+        if departure is None:
+            departure = "here"
+
+        #receives directions in the format of a response string from Google Maps API through the client
+        response = gmaps.getMycroftResponse(departure, destination)
+
+        #responds to the user with directions to where they want to go
+        self.speak(response)
 
 
 def create_skill():
